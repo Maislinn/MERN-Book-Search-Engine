a4e43af51e000b4e02edb355d7bb477218197fe1
Maislinn <maislinn.helfer@gmail.com>
2023-04-16 13:06:12 -0700

    debugging auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,7 +8,7 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -23,10 +23,13 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+    } catch (err) {
+      console.error(err.message);
       console.log('Invalid token');
       return res.status(400).json({ message: 'invalid token!' });
     }
+
+    return req;
 
     // send to next endpoint
     next();
