47741174984aa93b19a548ce0186119768984962
Maislinn <maislinn.helfer@gmail.com>
2023-04-16 13:09:34 -0700

    updating server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+//adding apollo server
+const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
+//add schemas and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+//require auth middleware
+const { authMiddleware } = require('./utils/auth');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,4 +34,5 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
